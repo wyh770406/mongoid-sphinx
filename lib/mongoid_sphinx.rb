837ae98ecd6a58e7ad31252310aa1eb33f2950f9
Matt Hodgson <matthodgson@me.com>
2011-01-04 15:52:44 -0500

    remove unneeded class

--- lib/mongoid_sphinx.rb
+++ lib/mongoid_sphinx.rb
@@ -2,6 +2,5 @@ require "mongoid"
 require "riddle"
 
 require 'mongoid_sphinx/configuration'
-require 'mongoid_sphinx/multi_attribute'
 require 'mongoid_sphinx/mongoid/identity'
 require 'mongoid_sphinx/mongoid/sphinx'
