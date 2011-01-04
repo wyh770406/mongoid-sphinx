5e4acd3664bedc085785df044d25517a961df595
Matt Hodgson <matthodgson@me.com>
2011-01-04 14:59:35 -0500

    fix attributes method

--- lib/mongoid_sphinx/mongoid/sphinx.rb
+++ lib/mongoid_sphinx/mongoid/sphinx.rb
@@ -5,12 +5,74 @@ module Mongoid
   module Sphinx
     extend ActiveSupport::Concern
     included do
+      SPHINX_TYPE_MAPPING = {
+        'Date' => 'timestamp',
+        'DateTime' => 'timestamp',
+        'Time' => 'timestamp',
+        'Float' => 'float',
+        'Integer' => 'uint',
+        'BigDecimal' => 'float',
+        'Boolean' => 'bool'
+      }
+      
       cattr_accessor :search_fields
+      cattr_accessor :search_attributes
     end
     
     module ClassMethods
-      def search_index(*fields)
-        self.search_fields = fields
+      def search_index(options={})
+        self.search_fields = options[:fields]
+        self.search_attributes = {}
+        options[:attributes].each do |attrib|
+          self.search_attributes[attrib] = SPHINX_TYPE_MAPPING[self.fields[attrib.to_s].type.to_s] || 'string'
+        end
+      end
+      
+      def self.sphinx_stream
+        STDOUT.sync = true # Make sure we really stream..
+        
+        puts '<?xml version="1.0" encoding="utf-8"?>'
+        puts '<sphinx:docset>'
+        
+        # Schema
+        puts '<sphinx:schema>'
+        self.search_fields.each do |key, value|
+          puts "<sphinx:field name=\"#{key}\"/>"
+        end
+        self.search_attributes.each do |key, value|
+          puts "<sphinx:attr name=\"#{key}\" type=\"#{value}\"/>"
+        end
+        # FIXME: What is this attribute?
+        puts '<sphinx:field name="classname"/>'
+        puts '<sphinx:attr name="csphinx-class" type="multi"/>'
+        puts '</sphinx:schema>'
+        
+        self.find.each do |document_hash|
+          sphinx_compatible_id = document_hash['_id'].to_s.to_i - 100000000000000000000000
+          
+          puts "<sphinx:document id=\"#{sphinx_compatible_id}\">"
+          # FIXME: Should we include this?
+          puts '<csphinx-class>'
+          puts MongoidSphinx::MultiAttribute.encode(self.to_s)
+          puts '</csphinx-class>'
+          puts "<classname>#{self.to_s}</classname>"
+          
+          self.search_fields.each do |key|
+            puts "<#{key}><![CDATA[[#{document_hash[key.to_s]}]]></#{key}>"
+          end
+          self.search_attributes.each do |key, value|
+            value = case value
+              when 'bool' : document_hash[key.to_s] ? 1 : 0
+              when 'timestamp' : document_hash[key.to_s].to_i
+              else document_hash[key.to_s].to_s
+            end 
+            puts "<#{key}><![CDATA[[#{value}]]></#{key}>"
+          end
+          
+          puts '</sphinx:document>'
+        end
+        
+        puts '</sphinx:docset>'
       end
       
       def search(query, options = {})
@@ -28,14 +90,12 @@ module Mongoid
         end
         
         if options.key?(:with)
-          client.filters.
           options[:with].each do |key, value|
             client.filters << Riddle::Client::Filter.new(key.to_s, value, false)
           end
         end
         
         if options.key?(:without)
-          client.filters.
           options[:without].each do |key, value|
             client.filters << Riddle::Client::Filter.new(key.to_s, value, true)
           end
