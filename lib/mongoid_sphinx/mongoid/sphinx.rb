26c044115ff278a3b61f178a9396478840e9fb7b
Matt Hodgson <matthodgson@me.com>
2011-02-26 15:44:27 -0500

    allow regular methods

--- lib/mongoid_sphinx/mongoid/sphinx.rb
+++ lib/mongoid_sphinx/mongoid/sphinx.rb
@@ -62,20 +62,20 @@ module Mongoid
         end
         puts '</sphinx:schema>'
         
-        self.all.each do |document_hash|
-          sphinx_compatible_id = document_hash['_id'].to_s.to_i - 100000000000000000000000
+        self.all.each do |document|
+          sphinx_compatible_id = document['_id'].to_s.to_i - 100000000000000000000000
           if sphinx_compatible_id > 0
             puts "<sphinx:document id=\"#{sphinx_compatible_id}\">"
             
             puts "<classname>#{self.to_s}</classname>"
             self.search_fields.each do |key|
-              puts "<#{key}><![CDATA[#{document_hash[key.to_s]}]]></#{key}>"
+              puts "<#{key}><![CDATA[#{document.send(key.to_s)}]]></#{key}>"
             end
             self.search_attributes.each do |key, value|
               value = case value
-                when 'bool' : document_hash[key.to_s] ? 1 : 0
-                when 'timestamp' : document_hash[key.to_s].to_i
-                else document_hash[key.to_s].to_s
+                when 'bool' : document.send(key.to_s) ? 1 : 0
+                when 'timestamp' : document.send(key.to_s).to_i
+                else document.send(key.to_s).to_s
               end 
               puts "<#{key}>#{value}</#{key}>"
             end
