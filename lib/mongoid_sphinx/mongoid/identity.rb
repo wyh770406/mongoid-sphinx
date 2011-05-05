4ff88449d33ef14b10a9f104badbb3fc1ce3b618
Matt Hodgson <matthodgson@me.com>
2011-05-05 11:51:37 -0400

    updates to identity

--- lib/mongoid_sphinx/mongoid/identity.rb
+++ lib/mongoid_sphinx/mongoid/identity.rb
@@ -1,19 +1,23 @@
 module Mongoid
   class Identity
     
-    protected
+    private
       
       # Return an id that is sphinx compatible
       def generate_id
-        while true
-          id = 100000000000000000000000 + rand(4294967294) # 4,294,967,295 is the theoretical max number of documents a 32 bit sphinx install can index
-          candidate = id.to_s
+        if @document.class.embedded?
+          id = BSON::ObjectId.new
+        else
+          while true
+            id = 100000000000000000000000 + rand(4294967294) # 4,294,967,295 is the theoretical max number of documents a 32 bit sphinx install can index
+            candidate = id.to_s
           
-          begin
-            @document.class.find(candidate) # Resource not found exception if available
-          rescue Mongoid::Errors::DocumentNotFound
-            id = BSON::ObjectId.from_string(candidate)
-            break
+            begin
+              @document.class.find(candidate) # Resource not found exception if available
+            rescue Mongoid::Errors::DocumentNotFound
+              id = BSON::ObjectId.from_string(candidate)
+              break
+            end
           end
         end
         @document.using_object_ids? ? id : id.to_s
